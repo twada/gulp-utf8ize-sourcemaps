1871b4cd6c9804ad03e86aac789db11085027341
Takuto Wada <takuto.wada@gmail.com>
2020-04-17 09:37:21 +0900

    feat: modernize codebase
    BREAKING CHANGE: drop support for old Node versions (Node < 10)
    Users of old Nodes should install gulp-derequire@2.x

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * gulp-utf8ize-sourcemaps
- * 
+ *
  * https://github.com/twada/gulp-utf8ize-sourcemaps
  *
  * Copyright (c) 2014-2020 Takuto Wada
@@ -9,34 +9,31 @@
  */
 'use strict';
 
-var utf8ize = require('utf8ize-sourcemaps');
-var through = require('through2');
-var BufferStreams = require('bufferstreams');
-var PluginError = require('plugin-error');
-var PLUGIN_NAME = 'gulp-utf8ize-sourcemaps';
+const utf8ize = require('utf8ize-sourcemaps');
+const through = require('through2');
+const BufferStreams = require('bufferstreams');
+const PluginError = require('plugin-error');
+const PLUGIN_NAME = 'gulp-utf8ize-sourcemaps';
+const transform = (code) => Buffer.from(utf8ize(code));
 
-var transform = function (code) {
-    return Buffer.from(utf8ize(code));
-};
-
-module.exports = function () {
-    return through.obj(function (file, encoding, callback) {
-        encoding = encoding || 'utf8';
-        if (file.isNull()) {
-            this.push(file);
-        } else if (file.isBuffer()) {
-            file.contents = transform(file.contents.toString(encoding));
-            this.push(file);
-        } else if (file.isStream()) {
-            file.contents = file.contents.pipe(new BufferStreams(function(err, buf, cb) {
-                if(err) {
-                    cb(new PluginError(PLUGIN_NAME, err, {showStack: true}));
-                } else {
-                    cb(null, transform(buf.toString(encoding)));
-                }
-            }));
-            this.push(file);
+module.exports = () => {
+  return through.obj(function (file, encoding, callback) {
+    encoding = encoding || 'utf8';
+    if (file.isNull()) {
+      this.push(file);
+    } else if (file.isBuffer()) {
+      file.contents = transform(file.contents.toString(encoding));
+      this.push(file);
+    } else if (file.isStream()) {
+      file.contents = file.contents.pipe(new BufferStreams((err, buf, cb) => {
+        if (err) {
+          cb(new PluginError(PLUGIN_NAME, err, { showStack: true }));
+        } else {
+          cb(null, transform(buf.toString(encoding)));
         }
-        callback();
-    });
+      }));
+      this.push(file);
+    }
+    callback();
+  });
 };
